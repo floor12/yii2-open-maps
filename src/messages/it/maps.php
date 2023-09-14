ff1005672832e6a250a335083c2d3f9ec7fe756e
floor12 <floor12@floor12.net>
2023-09-14 09:42:46 +0200

    IT lang added.

---
+++ src/messages/it/maps.php
@@ -0,0 +1,15 @@
+<?php
+return [
+    'Init latitude' => 'Latitudine iniziale',
+    'Init longitude' => 'Longitudine iniziale',
+    'Init zoom value' => 'Valore di zoom iniziale',
+    'Points' => 'Punti',
+    'Map creation' => 'Creazione della mappa',
+    'Map update' => 'Aggiornamento della mappa',
+    'Create map' => 'Crea mappa',
+    'Maps' => 'Mappe',
+    'Url slug' => 'Slug dell`URL',
+    'Map' => 'Mappa',
+    'Draw path' => 'Disegna percorso',
+    'Switch map mode' => 'Cambia modalità mappa',
+];
